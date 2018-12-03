a7f0148e4cf143329c209ab0a0cb618dcba75d59
Dawid Paprocki <dawid.paprocki90@gmail.com>
2018-12-04 00:07:51 +0100

    added progress bars & annimation for first block.

--- js2.js
+++ js2.js
@@ -22,3 +22,53 @@ hamburgerid.classList.toggle("change");
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
+
+
+$(document).ready(function() {
+
+  $('#example-1').progress_fnc();
+
+
+
+});
+
+
+(function($) {
+
+  $.fn.progress_fnc = function(options) {
+    var settings = $.extend({
+      type: 'start'
+    }, options);
+
+    var div = $(this);
+    var progress = div.find('.cssProgress');
+
+    progress.each(function() {
+      var self = $(this);
+      var progress_bar = self.find('.cssProgress-bar');
+      var progress_label = self.find('.cssProgress-label, .cssProgress-label2');
+      var label_move = self.find('.first75');
+      var progress_value = progress_bar.data('percent');
+      var percentage = parseInt(progress_value, 10) + '%';
+
+      progress_bar.css({'width': '0%', 'transition': 'none', '-webkit-transition': 'none', '-moz-transition': 'none'});
+
+      if(settings.type == 'start') {
+
+
+        progress_bar.animate({
+          width: percentage
+        }, {
+          duration: 1000,
+          step: function(x) {
+            progress_label.text(Math.round(x) + '%');
+
+          }
+        });
+
+      }
+
+    });
+  }
+
+}(jQuery));
